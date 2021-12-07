bece63a387121f4f2f74d39a0b90dadda259a7fe
Anderson Alves <andersonricardo.alves@gmail.com>
2021-12-07 19:49:04 +0000

    entities and tests

---
+++ domain/entity/credit_card_test.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestCreditCardNumber(t *testing.T) {
+	_, err := NewCreditCard("40000000000000000", "Jose da Silva", 12, 2024, 123)	                         
+	assert.Equal(t, "invalid credit card number", err.Error())
+	
+	_, err = NewCreditCard("5156010683573034", "Jose da Silva", 12, 2024, 123)	                         
+	assert.Nil(t, err)
+}
+
+func TestCreditCardExpirationMonth(t *testing.T) {
+	_, err := NewCreditCard("5156010683573034", "Jose da Silva", 13, 2024, 123)	                         
+	assert.Equal(t, "invalid expiration month", err.Error())
+	
+	_, err = NewCreditCard("5156010683573034", "Jose da Silva", 0, 2024, 123)	                         
+	assert.Equal(t, "invalid expiration month", err.Error())	
+	
+	_, err = NewCreditCard("5156010683573034", "Jose da Silva", 11, 2024, 123)	                         
+	assert.Nil(t, err)
+}
+
+func TestCreditCardExpirationYear(t *testing.T) {
+	lastYear := time.Now().AddDate(-1, 0, 0)
+
+	_, err := NewCreditCard("5156010683573034", "Jose da Silva", 11, lastYear.Year(), 123)	                         
+	assert.Equal(t, "invalid expiration year", err.Error())
+}
